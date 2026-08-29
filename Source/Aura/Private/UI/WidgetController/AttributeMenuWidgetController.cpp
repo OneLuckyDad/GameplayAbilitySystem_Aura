ef954442d02ac5bbfbbd121c5dfe541f4eccc0fc
OneLuckyDad <luca.difranco.87@gmail.com>
2026-08-29 23:05:56 +0100

    Attribute menu widget controller

---
+++ Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -0,0 +1,17 @@
+// Copyright OneLuckyDad
+
+#include "UI/WidgetController/AttributeMenuWidgetController.h"
+
+void UAttributeMenuWidgetController::BindCallbacksToDependencies()
+{
+	Super::BindCallbacksToDependencies();
+	
+	
+}
+
+void UAttributeMenuWidgetController::BroadCastInitialValues()
+{
+	Super::BroadCastInitialValues();
+	
+	
+}
---
+++ Source/Aura/Public/UI/WidgetController/AttributeMenuWidgetController.h
@@ -0,0 +1,18 @@
+// Copyright OneLuckyDad
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/WidgetController/AuraWidgetController.h"
+#include "AttributeMenuWidgetController.generated.h"
+
+UCLASS()
+class AURA_API UAttributeMenuWidgetController : public UAuraWidgetController
+{
+	GENERATED_BODY()
+	
+public:
+	virtual void BindCallbacksToDependencies() override;
+	virtual void BroadCastInitialValues() override;
+	
+};
